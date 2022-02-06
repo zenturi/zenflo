04e9545712d135070beaeea518b39e2a0868587b
Damilare Akinlaja <darmie@akinlaja.com>
2022-02-06 19:12:17 +0100

    [WIP] fix event emitter to support nested listeners

--- src/zenflo/lib/EventEmitter.hx
+++ src/zenflo/lib/EventEmitter.hx
@@ -3,27 +3,30 @@ package zenflo.lib;
 // import hx.concurrent.Future.FutureResult;
 import haxe.CallStack;
 import haxe.ds.Map;
-// import tink.core.Callback;
-// import tink.core.Callback.CallbackLink;
-// import tink.core.Pair.MPair;
-// import tink.core.Signal;
-// import tink.core.Signal.SignalTrigger;
-// import hx.concurrent.event.AsyncEventDispatcher;
-// import hx.concurrent.event.SyncEventDispatcher;
-// import hx.concurrent.executor.Executor;
-// import rx.Subject;
-import rx.subjects.Replay as Subject;
+
+import rx.Subject;
 import rx.Observer;
 
 using StringTools;
 
-class EventEmitter {
-	var subjects:Map<String, {subject:Subject<Array<Any>>, once:Bool,  handler:(data:Array<Any>) -> Void}>;
 
-	var listeners:Map<String, Array<Observer<Array<Any>>>>;
+/**
+ * Todo: This Event emitter implementation is not perfect. It doesn't work for nested listeners. 
+ * [NEED A FIX]
+ */
+
+
+typedef SubjectMap = {subject:Subject<Array<Any>>, once:Bool,  handler:(data:Array<Any>) -> Void};
+
+
+
+class EventEmitter {
+	var subjects:Map<String, Array<SubjectMap>>;
+
+	var listeners:Map<String, Array<(data:Array<Any>) -> Void>>;
 
 	public function new() {
-		subjects = new Map<String, {subject:Subject<Array<Any>>, once:Bool,  handler:(data:Array<Any>) -> Void}>();
+		subjects = new Map<String, Array<SubjectMap>>();
 		listeners = new Map();
 	}
 
@@ -35,30 +38,39 @@ class EventEmitter {
 		final fnName = createName(name);
 		if (this.subjects.exists(fnName)) {
 			final x = [for (v in data) v];
-			final f = this.subjects.get(fnName);
-			if (f != null && f.subject != null) {
-				f.subject.on_next(x);
-				if (f.once) {
-					f.subject.unsubscribe();
-					this.subjects.remove(fnName);
-					this.listeners.clear();
+			final fs = this.subjects.get(fnName);
+
+			for(f in fs){
+				if (f != null && f.subject != null) {
+					f.subject.on_next(x);
 				}
 			}
+
+			Lambda.iter(fs, (f)->{
+				if (f.once) {
+					f.subject.unsubscribe();
+					for(l in this.listeners.get(fnName)){
+						if(l == f.handler){
+							this.listeners.get(fnName).remove(l);
+						}
+					}
+				}
+			});
+			
 		}
 	}
 
 	public function on(name:String, handler:(data:Array<Any>) -> Void, once:Bool = false) {
 		final fnName = createName(name);
 		if (!this.subjects.exists(fnName)) {
-			this.subjects.set(fnName, {subject: new Subject(), once: once, handler: handler});
+			this.subjects.set(fnName, [{subject: Subject.create(), once: once, handler: handler}]);
 		}
 		if(!this.listeners.exists(fnName)) listeners.set(fnName, []);
-		final f = this.subjects.get(fnName);
-		if (f != null && f.subject != null) {
-			final o = Observer.create(null, null, handler);
-			f.subject.subscribe(o);
-			listeners.get(fnName).push(o);
-		}
+		final fs = this.subjects.get(fnName);
+		final sub = Subject.create();
+		sub.subscribe(Observer.create(null, null, handler));
+		fs.push({subject: sub, once: once, handler: handler});
+		listeners.get(fnName).push(handler);
 	}
 
 	public function once(name:String, handler:(data:Array<Any>) -> Void) {
@@ -67,7 +79,9 @@ class EventEmitter {
 
 	public function removeAllListeners() {
 		for (k => v in this.subjects) {
-			v.subject.unsubscribe();
+			Lambda.iter(v, (f)->{
+				f.subject.unsubscribe();
+			});
 			this.subjects.remove(k);
 		}
 	}
@@ -75,9 +89,13 @@ class EventEmitter {
 	public function removeListener(name:String, handler:(data:Array<Any>) -> Void){
 		final fnName = createName(name);
 		if(this.subjects.exists(fnName)){
-			if (this.subjects[fnName].handler  == handler){
-				this.subjects[fnName].subject.unsubscribe();
-			}
+			final fs = this.subjects[fnName];
+			Lambda.iter(fs, (f)->{
+				if(f.handler == handler){
+					f.subject.unsubscribe();
+					fs.remove(f);
+				}
+			});
 		}
 	}
 }
